b29e24b0dcee476b9c79e468cd0377a4351b7140
Pavel Mishagin <xstyle71@gmail.com>
2022-01-14 16:40:18 +0300

    Add onboarding flow

---
+++ Time/Shared/Utils/Extensions/Color+extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Color+extensions.swift
+//  Time (iOS)
+//
+//  Created by Павел Мишагин on 14.01.2022.
+//
+
+
+
+import SwiftUI
+
+extension Color {
+    public static var Tgray: Color {
+        return Color(UIColor(named: "gray") ?? .gray)
+    }
+}
