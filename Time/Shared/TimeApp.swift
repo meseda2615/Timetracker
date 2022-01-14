27b6b422348bc4b8b4e85f9653a9bdfa76bc0a73
Pavel Mishagin <xstyle71@gmail.com>
2022-01-14 16:29:30 +0300

    make init commit

---
+++ Time/Shared/TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeApp.swift
+//  Shared
+//
+//  Created by Павел Мишагин on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
